34a2c03089aa4824faad79bf7ab2de63dd89beb1
SIGMOID <1535899184@qq.com>
2022-12-12 23:55:45 +0800

    Update server.c

--- cs-base-udp/server.c
+++ cs-base-udp/server.c
@@ -7,43 +7,49 @@
 #include <netinet/in.h>
 
 #define BUF_SIZE 100
+#define RES_SIZE 50
 #define  err_log(errlog) do{ perror(errlog); exit(1); }while(0)
 
 int main(){
-
-    // 1.创建套接字
-    int sock;
+    int serv_sock;
     struct sockaddr_in serv_addr, clnt_addr;
 
-    if((sock = socket(PF_INET, SOCK_DGRAM, 0))<0){
+    // 1.创建套接字
+    if((serv_sock = socket(PF_INET, SOCK_DGRAM, 0))<0){
         err_log("fail to sock.");
     }
     printf("服务器创建套接字成功！\n");
     sleep(3);
 
-    // 2. 绑定套接字
+    
     memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
     serv_addr.sin_family = AF_INET;  //使用IPv4地址
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); //自动获取IP地址
     serv_addr.sin_port = htons(1234);  //端口
-    if(bind(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))<0){
+
+    // 2. 绑定套接字
+    if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))<0){
         err_log("fail to bing.");
     }
     printf("服务器绑定套接字成功.\n");
     sleep(3);
 
-    // 3. 接收客户端请求
+
     socklen_t clnt_addr_size = sizeof(clnt_addr);
     char buffer[BUF_SIZE];  //缓冲区
-    while(1){
-        int str_len = recvfrom(sock, buffer, BUF_SIZE, 0, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+    while(true){
+        // 3. 接收客户端请求
+        int str_len = recvfrom(serv_sock, buffer, BUF_SIZE, 0, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
         printf("从客户端接收消息：%s\n", buffer);
         sleep(3);
 
-        printf("服务器将消息返回给客户端.\n");
-        sendto(sock, buffer, str_len, 0, (struct sockaddr*)&clnt_addr, clnt_addr_size);
+        // 4. 向客户端发送消息
+        char response[RES_SIZE] = "OK，信息已收到。";
+        sendto(serv_sock, response, RES_SIZE, 0, (struct sockaddr*)&clnt_addr, clnt_addr_size);
+        printf("向客户端发送消息：%s\n", response);
     }
 
-    close(sock);
+    close(serv_sock);
+
     return 0;
 }
